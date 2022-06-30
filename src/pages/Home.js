e343ffd9a45b5809d2ca5862457267bd90610f6c
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-06-30 01:23:19 -0300

    utilizando stylesheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,58 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 export function Home() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Tufão gato branco </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Felipe </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New Skills"
+        placeholderTextColor="#555"
+      />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}> Add </Text>
+      </TouchableOpacity>
+      <Text style={[styles.title, {marginTop: 20}]}> My Skills </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1a1e25',
+    color: '#fff',
+    padding: 15,
+    marginTop: 30,
+    borderRadius: 7,
+    fontSize: 18,
+  },
+  button: {
+    backgroundColor: '#a370f0',
+    padding: 20,
+    borderRadius: 7,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
